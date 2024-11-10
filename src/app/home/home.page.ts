acb7749fc6f5c91a79044778a496e80f5c42db02
JesusPalacio <jesusdanielpalacioavila50@gmail.com>
2024-11-09 19:29:15 -0500

    Formulario listo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  petsForm: FormGroup;
+  userId!: string;
 
-  constructor() {}
+  constructor(
+  private formBuilder: FormBuilder, 
+  ){
+
+  this.petsForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    breed: ['', Validators.required],
+    age: ['', Validators.required],
+    birthdate: ['', Validators.required],
+  });
+  }
+
+  ngOnInit(): void {
+  }
 
 }
