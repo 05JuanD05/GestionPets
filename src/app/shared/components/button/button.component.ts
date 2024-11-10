023fc1e0fd9d45e6a7e5719446de93ef949b7b89
Juan D Bertel <berteljuan77@gmail.com>
2024-11-10 10:59:14 -0500

    CSS listo, falta la opcion de camara

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
-type colorButton = "success" | "danger";
+type colorButton = "success" | "danger" | "primary";
 type buttonType = "button" | "submit";
 
 @Component({
