412ad1df6df2f70b5a5f7d01334f9e9d5554b2b8
IvanGloria <davidgloria101@gmail.com>
2024-11-11 14:17:15 -0500

    estilo de login casi listo

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -10,6 +10,7 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+rememberMe: boolean = false;
 public email!: FormControl;
 public password!: FormControl;
 public loginform!: FormGroup;
