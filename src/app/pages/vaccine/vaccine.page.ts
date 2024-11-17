f7f5079e4871f7a9cd9adc9f24e4e5afcb5f8773
Edward <edward11.eagm@gmail.com>
2024-11-17 00:36:29 -0500

    registroVacuna conectada a la bd listo falta el capasitor de cartificado

--- src/app/pages/vaccine/vaccine.page.ts
+++ src/app/pages/vaccine/vaccine.page.ts
@@ -1,22 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { DatePipe } from '@angular/common';
+import { VaccineService } from 'src/app/shared/services/vaccineS/vaccine.service';
 
 @Component({
   selector: 'app-vaccine',
   templateUrl: './vaccine.page.html',
   styleUrls: ['./vaccine.page.scss'],
-  providers: [DatePipe]
 })
 export class VaccinePage implements OnInit {
   public name!: FormControl;
   public date!: FormControl;
   public certificado!: FormControl;
   public agregarForm!: FormGroup;
-  public vacunas: any[] = [];
-  public editIndex: number | null = null;
+  public vacunas: any[] = []; // Lista para almacenar las vacunas
+  public editIndex: string | null = null; // ID de la vacuna que se va a editar
 
-  constructor(private datePipe: DatePipe) { }
+  constructor(private vaccineService: VaccineService) {}
 
   ngOnInit() {
     this.name = new FormControl('');
@@ -27,35 +26,43 @@ export class VaccinePage implements OnInit {
       date: this.date,
       certificado: this.certificado,
     });
+
+    // Cargar vacunas desde Firebase
+    this.vaccineService.obtenerVacunas().subscribe((data) => {
+      this.vacunas = data;
+    });
   }
 
   agregarVacuna() {
-    const formattedDate = this.datePipe.transform(this.date.value, 'dd/MM/yyyy'); // Asegura que la fecha esté formateada
     const nuevaVacuna = {
       name: this.name.value,
-      date: formattedDate,
+      date: this.date.value,
       certificado: this.certificado.value,
     };
 
-    if (this.editIndex !== null) {
-      this.vacunas[this.editIndex] = nuevaVacuna;
-      this.editIndex = null;
+    if (this.editIndex) {
+      // Actualizar vacuna
+      this.vaccineService.actualizarVacuna(this.editIndex, nuevaVacuna).then(() => {
+        this.editIndex = null; // Restablece el índice después de actualizar
+      });
     } else {
-      this.vacunas.push(nuevaVacuna);
+      // Agregar nueva vacuna
+      this.vaccineService.agregarVacuna(nuevaVacuna).then(() => {
+        this.agregarForm.reset(); // Resetea el formulario después de agregar
+      });
     }
-
-    this.agregarForm.reset();
   }
 
-  eliminarVacuna(index: number) {
-    this.vacunas.splice(index, 1);
+  eliminarVacuna(id: string) {
+    this.vaccineService.eliminarVacuna(id).then(() => {
+      console.log('Vacuna eliminada');
+    });
   }
 
-  editarVacuna(index: number) {
-    const vacuna = this.vacunas[index];
+  editarVacuna(vacuna: any) {
     this.name.setValue(vacuna.name);
     this.date.setValue(vacuna.date);
     this.certificado.setValue(vacuna.certificado);
-    this.editIndex = index;
+    this.editIndex = vacuna.id; // Guarda el ID para actualizar
   }
 }
