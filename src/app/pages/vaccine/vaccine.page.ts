76f4676d1924a3c680a007effdf413cba2d87b43
Luis Garavito Hernandez <luisdagh11@gmail.com>
2024-11-17 23:38:57 -0500

    Haciendo pull de la rama vaccine

--- src/app/pages/vaccine/vaccine.page.ts
+++ src/app/pages/vaccine/vaccine.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-
+import { FilePicker } from '@capawesome/capacitor-file-picker';
+import { FileOpener } from '@capawesome-team/capacitor-file-opener';
 @Component({
   selector: 'app-vaccine',
   templateUrl: './vaccine.page.html',
@@ -13,10 +14,62 @@ export class VaccinePage implements OnInit {
   public certificado!: FormControl;
   public id: string = "";
   public agregarForm!: FormGroup;
+  imageSrc1: any;
+  imageSrc2: any;
+  filePath: any;
 
   constructor() { }
 
   ngOnInit() {
+  }
+  pickFiles = async () => {
+    const result = await FilePicker.pickFiles({
+      types: ['image/png'],
+      readData: true
+    });
+
+    this.filePath = result.files[0].path;
+    this.imageSrc1 = 'data:image/jpeg;base64,' + result.files[0].data;
+  }
+
+  pickImages = async () => {
+    const result = await FilePicker.pickImages({
+      readData: true
+    });
+
+    this.imageSrc1 = 'data:image/jpeg;base64,' + result.files[0].data;
+    this.imageSrc2 = 'data:image/jpeg;base64,' + result.files[1].data;
+  };
+
+  pickPDFiles = async () => {
+    const result = await FilePicker.pickFiles({
+      types: ['aplication/pdf'],
+      readData: true
+    });
+
+    result ? this.filePath = result.files[0].path : alert("No file path")
+  }
+
+  pickVideo = async () => {
+    try {
+      const result = await FilePicker.pickFiles({
+        types: ['video/mp4']
+      });
+
+      this.filePath = result.files[0].path;
+    } catch (error) {
+      alert(JSON.stringify(error))
+    }
+  };
+
+  openFile = async () => {
+    try {
+      await FileOpener.openFile({
+        path: this.filePath,
+      });
+    } catch (error) {
+      alert(error)
+    };
   }
 
 }
